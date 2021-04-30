4cde67a0a1e169731786115973e5392a43c080e0
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-30 15:34:42 +0300

    add the profile section

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from . import main
 from flask_login import login_required
+from .forms import UpdateProfile
+from .. import db
 
 
 @main.route('/')
@@ -18,4 +20,33 @@ def blogs():
 @login_required
 def add_blog():
     return render_template(add_blog.html)
+
     
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
