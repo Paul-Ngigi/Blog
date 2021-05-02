b06f4dd8fdb8272d69cfd928d8ae6882831b4c14
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-02 15:19:25 +0300

    add a blog form

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,6 +7,13 @@ class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
     
+
+class BlogForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    post = TextAreaField('Pitch', validators=[Required()])
+    submit = SubmitField('Post Blog')
+
+    
 class CommentsForm(FlaskForm):
     comment = TextAreaField('Comment')
     submit = SubmitField('submit')
