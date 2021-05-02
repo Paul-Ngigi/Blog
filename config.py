cae478fe6d026484facaf8b6ac0d769659942e1a
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-02 14:06:48 +0300

    update a test class

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://root:root@localhost/blog'
+    QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     # email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
@@ -28,6 +29,9 @@ class DevConfig(Config):
     """
     DEBUG = True
     
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://andrewjohn:andy1234@localhost/blog_test'
+
 
 config_option = {
     'production': ProdConfig,
